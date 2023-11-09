adeb92e3a332e2ffaafb342f7b23aa857f396f91
Crispen Gari <crispengari@gmail.com>
2023-11-09 10:11:12 +0200

    testing

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from app import db, app
-from blueprints.user import blueprint as user
-from blueprints.todo import blueprint as todo
+from app import app
 
-app.register_blueprint(user, url_prefix="/users")
-app.register_blueprint(todo, url_prefix="/todos")
+# from blueprints.user import blueprint as user
+# from blueprints.todo import blueprint as todo
+
+
+@app.route("/", methods=["GET", "POST"])
+def hi():
+    return "Hi there", 200
+
+
+# app.register_blueprint(user, url_prefix="/users")
+# app.register_blueprint(todo, url_prefix="/todos")
 
 if __name__ == "__main__":
     with app.app_context():
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,29 @@
 from app import db
 
 
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column("id", db.Integer(), primary_key=True, nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean(), nullable=False, unique=False, default=False)
+
+    def __init__(self, title: str):
+        self.title = title
+
+    def json(self):
+        return {
+            "title": self.title,
+            "completed": self.completed,
+            "id": self.id,
+        }
+
+    def __repr__(self):
+        return "<Todo %r>" % self.title
+
+    def __str__(self):
+        return "<Todo %r>" % self.title
+
+
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column("id", db.Integer(), primary_key=True, nullable=False)
